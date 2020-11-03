2afb9693cdd068426d61db561271af6b9b759dcf
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2020-11-03 13:54:51 +0400

    Add files via upload

---
+++ sigrdzis_erteulebi.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // standartuli biblioteka
+
+using namespace std;
+
+int main() {
+    int f, d; // shesatani cvladebi
+
+    cin >> f >> d; //shetanis operacia
+    cout << f * 30.48 + d * 2.54; // gamosatvleli algorithmi
+}
