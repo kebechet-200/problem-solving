aa3b32f92e0c742e7bbd3cbe6e7b74a87fb686ed
Lasha Suliashvili <58426811+kebechet-200@users.noreply.github.com>
2020-11-03 13:41:49 +0400

    sashualos gamotvla

---
+++ sashualos_gamotvla.cpp
@@ -0,0 +1,15 @@
+// sashualos gamotvla or ricxvze.
+
+#include <bits/stdc++.h> // radgan gamotvla davamrgvalot or atwiladamde.
+
+using namespace std; // gamoiyene std saxelta sivrce romelic moicavs standartul operaciebs (cin/cout setprecision da sxva)
+
+int main() {
+    int f,s; // momxmareblis shesatani cvladebi
+    float c; // sashualos operaciis cvladi
+
+    cin >> f >> s; //sheyavs momxmarebels monacemebi
+    c = (double) (f + s) / 2; // sashualos gamosatvleli standartuli algoritmi
+    cout << fixed; // shesruldes operacia erti atwiladis gamosatanad mdzimis shemdeg.
+    cout << setprecision(1) << c << endl; // pasuxis gamotana. setprecision(1) radgan pasuxi damrgvaldes mdzimis(wertilis) shemdeg erti erteulit.
+}
